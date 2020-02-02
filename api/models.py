2d1eaf0a344a3b67bf2ad9c9d37de3647a5b6707
Debbie Milburn <debbiemilburn2@gmail.com>
2020-02-02 15:32:45 -0500

    Add initial ShortenedUrl model and unit tests

--- api/models.py
+++ api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+import random
+import string
 
-# Create your models here.
+
+class ShortenedUrl(models.Model):
+	slug = models.SlugField(unique=True, max_length=255)
+	url = models.URLField(max_length=255)
+	created_on = models.DateTimeField(auto_now_add=True)
+	last_modified = models.DateTimeField(auto_now=True)
+
+	def save(self, *args, **kwargs):
+		if not self.slug:
+			self.slug = ShortenedUrl.generate_slug()
+		super().save(*args, **kwargs)
+
+	@classmethod
+	def generate_slug(cls):
+		generated_slug = None
+		while not generated_slug:
+			random_slug = "".join(random.choice(string.ascii_lowercase + string.digits) for _ in range(10))
+			if not ShortenedUrl.objects.filter(slug=random_slug).exists():
+				generated_slug = random_slug
+
+		return generated_slug
+
+
--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from django.db.utils import IntegrityError
+from api.models import ShortenedUrl
 
-# Create your tests here.
+class ShortenedUrlTestCase(TestCase):
+	def test_save(self):
+		url = "google.com"
+		slug = "i_like_bananas"
+		self.assertEqual(ShortenedUrl.objects.count(), 0)
+		ShortenedUrl.objects.create(url=url, slug=slug)
+		self.assertEqual(ShortenedUrl.objects.count(), 1)
+		shortened_url = ShortenedUrl.objects.first()
+		self.assertEqual(shortened_url.url, url)
+		self.assertEqual(shortened_url.slug, slug)
+
+	def test_save_slug_not_unique(self):
+		url = "google.com"
+		different_url = "wikipedia.com"
+		slug = "i_like_bananas"
+		self.assertEqual(ShortenedUrl.objects.count(), 0)
+		ShortenedUrl.objects.create(url=url, slug=slug)
+		self.assertEqual(ShortenedUrl.objects.count(), 1)
+
+		with self.assertRaises(Exception) as error:
+			ShortenedUrl.objects.create(url=different_url, slug=slug)
+		self.assertEqual(IntegrityError, type(error.exception))
+
+
+	def test_save_autogenerate_slug(self):
+		url = "google.com"
+		self.assertEqual(ShortenedUrl.objects.count(), 0)
+		shortened_url = ShortenedUrl.objects.create(url=url)
+		self.assertEqual(ShortenedUrl.objects.count(), 1)
+		self.assertIsNotNone(shortened_url.slug)
