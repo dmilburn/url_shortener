21ed6698958c77d8dea1e5f21b349736eec539e9
Debbie Milburn <debbiemilburn2@gmail.com>
2020-02-03 20:01:08 -0500

    Add comment to help add future URLs

--- url_shortener/urls.py
+++ url_shortener/urls.py
@@ -1,6 +1,11 @@
 from django.urls import include, path
 from api.views import redirect_slug
 
+'''
+Future devs: be wary of adding new URLs to this file without having them nested like the "api" paths
+In order to redirect any short link, we have to match any string at the highest level
+'''
+
 urlpatterns = [
 	path('api/', include('api.urls')),
 	path('<str:slug>', redirect_slug)
